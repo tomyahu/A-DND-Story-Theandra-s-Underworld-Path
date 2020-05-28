284a74c5f2f03edb1f534478d1c3dda040233b74
Tomás Ahumada <tomyahu@gmail.com>
2020-05-28 14:53:47 -0400

    restructured project

---
+++ src/items/Item.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var item_name = ""
+var description = ""
+var weight = 0
+var value = 0
+
+# getters
+func get_name():
+	return name
+
+func get_description():
+	return description
+
+func get_weight():
+	return weight
+
+func get_value():
+	return value
