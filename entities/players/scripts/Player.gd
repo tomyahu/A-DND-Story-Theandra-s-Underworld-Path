42c22e3c50d3c9d95be90b541f5a49f71c7eadc9
Tomás Ahumada <tomyahu@gmail.com>
2020-05-28 14:45:50 -0400

    added getters to character and player

--- entities/players/scripts/Player.gd
+++ entities/players/scripts/Player.gd
@@ -12,3 +12,41 @@ var levels_rogue = 0
 var levels_sorcerer = 0
 var levels_warlock = 0
 var levels_wizard = 0
+
+
+#getters
+func get_barbarian_levels():
+	return levels_barbarian
+
+func get_bard_levels():
+	return levels_bard
+
+func get_cleric_levels():
+	return levels_cleric
+
+func get_druid_levels():
+	return levels_druid
+
+func get_fighter_levels():
+	return levels_fighter
+
+func get_monk_levels():
+	return levels_monk
+
+func get_paladin_levels():
+	return levels_paladin
+
+func get_ranger_levels():
+	return levels_ranger
+
+func get_rogue_levels():
+	return levels_rogue
+
+func get_sorcerer_levels():
+	return levels_sorcerer
+
+func get_warlock_levels():
+	return levels_warlock
+
+func get_wizard_levels():
+	return levels_wizard
--- entities/scripts/Character.gd
+++ entities/scripts/Character.gd
@@ -13,6 +13,10 @@ var stat_int = 10
 var stat_wis = 10
 var stat_cha = 10
 
+var speed = 30
+var climb_speed = 0
+var swim_speed = 0
+var fly_speed = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,3 +26,58 @@ func _ready():
 # gets the armor class of a character
 func get_ac():
 	return 10 + CalculationLib.get_stat_modifier(stat_dex)
+
+# getters
+func get_max_hp():
+	return max_hp
+
+func get_hp():
+	return hp
+
+func get_str():
+	return stat_str
+
+func get_dex():
+	return stat_dex
+
+func get_con():
+	return stat_con
+
+func get_int():
+	return stat_int
+
+func get_wis():
+	return stat_wis
+
+func get_cha():
+	return stat_cha
+
+func get_str_mod():
+	return CalculationLib.get_stat_modifier(get_str())
+
+func get_dex_mod():
+	return CalculationLib.get_stat_modifier(get_dex())
+
+func get_con_mod():
+	return CalculationLib.get_stat_modifier(get_con())
+
+func get_int_mod():
+	return CalculationLib.get_stat_modifier(get_int())
+
+func get_wis_mod():
+	return CalculationLib.get_stat_modifier(get_wis())
+
+func get_cha_mod():
+	return CalculationLib.get_stat_modifier(get_cha())
+
+func get_speed():
+	return speed
+
+func get_climb_speed():
+	return climb_speed
+
+func get_swim_speed():
+	return swim_speed
+
+func get_fly_speed():
+	return fly_speed
